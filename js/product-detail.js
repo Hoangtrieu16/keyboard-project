af619073a9c2f418c7b6857f35d72a17d68911b7
Nguyễn Hoàng Triệu <hope.trieu2803@gmail.com>
2023-08-05 23:57:49 +0700

    update page product detail

---
+++ js/product-detail.js
@@ -0,0 +1,13 @@
+// var $ = jQuery;
+
+$(document).ready(function() {
+ 
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+ 
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+ 
+});
