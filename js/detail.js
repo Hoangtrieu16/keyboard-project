3f07d133fcc7e3319eb5c2077bdd46bc571db558
sutoan <93644983+sutoan@users.noreply.github.com>
2023-08-22 16:34:57 +0700

    pre-order
    pre-order

--- js/detail.js
+++ js/detail.js
@@ -12,21 +12,21 @@ document.addEventListener('mousemove', showImgContent);
 
 
 // Modal pro-detail
-const popup = document.querySelector('#popup_preOrder');
-const closebtn = document.querySelector('.close_modal');
-const body = document.querySelector('body');
-const modal = document.querySelector('#modal_preOrder');
+// const popup = document.querySelector('#popup_preOrder');
+// const closebtn = document.querySelector('.close_modal');
+// const body = document.querySelector('body');
+// const modal = document.querySelector('#modal_preOrder');
 
-popup.addEventListener('click', () =>{
-  modal.style.display = 'block';
-  modal.style.transition = "0.5s all linear"
-  body.style.background = "#00000050"
-  body.style.transition = "0.5s all linear"
-})
+// popup.addEventListener('click', () =>{
+//   modal.style.display = 'block';
+//   modal.style.transition = "0.5s all linear"
+//   body.style.background = "#00000050"
+//   body.style.transition = "0.5s all linear"
+// })
 
-closebtn.addEventListener('click', () => {
-  modal.style.display="none";
-  body.style.background = "#fff"
-  body.style.transition = "0.5s all linear"
+// closebtn.addEventListener('click', () => {
+//   modal.style.display="none";
+//   body.style.background = "#fff"
+//   body.style.transition = "0.5s all linear"
 
-})
+// })
